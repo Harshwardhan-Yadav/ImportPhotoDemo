33e79124fd019586fff9eccedac1cd6b53042dad
Harshwardhan_Yadav <harshempire230900@gmail.com>
2021-10-12 23:03:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/importphotodemo/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.importphotodemo;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.Manifest.permission;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void getPhoto(){
+        Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent,1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==1){
+            if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                getPhoto();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1 && resultCode==RESULT_OK && data!=null){
+            try{
+                Uri image=data.getData();
+                Bitmap bitmap=MediaStore.Images.Media.getBitmap(this.getContentResolver(),image);
+                ImageView imageView=findViewById(R.id.imageView);
+                imageView.setImageBitmap(bitmap);
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{permission.READ_EXTERNAL_STORAGE},1);
+        }
+        else{
+            getPhoto();
+        }
+    }
+}
